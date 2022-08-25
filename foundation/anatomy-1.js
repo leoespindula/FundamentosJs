c8cee5d2c8a3d9d226d72df21603ea0a282fbf51
leoespindula <espindulasystem.dev@gmail.com>
2022-08-25 14:49:34 -0300

    update

--- foundation/anatomy-1.js
+++ foundation/anatomy-1.js
@@ -1,5 +1,27 @@
 // Function Declaration
 
 function sayHello(){
-    
+    console.log('Hello')
 }
+
+sayHello()
+
+function sayHelloTo(name){
+    console.log('Hello ' + name)
+    console.log(`Olá ${name}`)
+}
+
+sayHelloTo('Mike')
+
+function returnHi(){
+    return 'Olaa!'
+}
+let saudacao = returnHi()
+console.log(saudacao)
+console.log(returnHi())
+
+function returnHiTo(name){
+    return `Olá ${name}!`
+}
+
+console.log(returnHiTo('Jhon'))
