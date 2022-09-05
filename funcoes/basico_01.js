15d77657d36fd97ef1ccc08eafad7305d956a36c
leoespindula <espindulasystem.dev@gmail.com>
2022-09-05 17:13:44 -0300

    ...

---
+++ funcoes/basico_01.js
@@ -0,0 +1,26 @@
+let a = 4
+console.log(a)
+
+//Function
+function bomDia(){
+    console.log('Bom Dia!')
+}
+
+bomDia()
+
+//Function expression 
+const boaTarde = function (){
+    console.log('Boa Tarde!')
+}
+
+boaTarde()
+
+function somar(a, b){
+    return a + b
+}
+
+let resultado = somar(3)
+console.log(resultado)
+
+resultado = somar (3, 6, 5, 6, 7, 8)
+console.log(resultado)
---
+++ functional/map-filter-reduce.js
@@ -0,0 +1,23 @@
+const students = [
+    {name :'Jake', score:6.4},
+    {name:'Emma', score:8.6},
+    {name:'Susan', score:9.4},
+    {name:'Peter',score:9.1},
+]
+
+const greatStudent = student => student.score>= 9
+const getScore = el => el.score
+const avg = (acc, el, i, array) => {
+    if(i === array.length - 1) {
+        return (acc + el) / array.length
+    }else {
+        return acc + el
+    }
+} 
+
+console.log(
+    students
+    .filter(greatStudent)
+    .map(getScore)
+    .reduce(avg)
+)
