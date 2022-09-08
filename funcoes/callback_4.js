0e8f2c410b156faaaf8f68457518a1dd61ee2788
leoespindula <espindulasystem.dev@gmail.com>
2022-09-08 16:56:47 -0300

    Update callback_4.js

--- funcoes/callback_4.js
+++ funcoes/callback_4.js
@@ -18,3 +18,21 @@ const nomeItemValido = carrinho
     .map(getNome)
 
 console.log(nomeItemValido)
+
+Array.prototype.meuFilter = function(fn) {
+    const novoArray = []
+
+    for(let i = 0; i < this.length; i++){
+        if(fn(this[i], i, this)) {
+            novoArray.push(this[i])
+        }
+    }
+
+    return novoArray
+}
+
+const nomeItensValido2 = carrinho
+    .meuFilter(qtdeMaiorQueZero)
+    .map(getNome)
+
+console.log(nomeItensValido2)
