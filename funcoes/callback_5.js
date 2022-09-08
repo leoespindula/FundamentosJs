00a2ff8fcfd9506b4ade9700ec34b81fcd562378
leoespindula <espindulasystem.dev@gmail.com>
2022-09-08 17:38:15 -0300

    Create callback_5.js

---
+++ funcoes/callback_5.js
@@ -0,0 +1,21 @@
+const carrinho = [
+    {nome:'Caneta', qtde: 10, preco: 7.99},
+    {nome:'Impressora', qtde: 0, preco: 649.50},
+    {nome:'Caderno', qtde: 4, preco: 27.10},
+    {nome:'Lapis', qtde: 3, preco: 5.82},
+    {nome:'Tesoura', qtde: 1, preco: 19.20}
+]
+
+const getTotal = item => item.qtde * item.preco
+const somar = (acc, el) => {
+    console.log(`R$${acc} + R$${el}`)
+    return acc + el
+} 
+
+const totais = carrinho.map(getTotal)
+
+console.log(totais)
+const totalGeral = totais.reduce(somar)
+
+
+console.log(`Valor total do carrinho: R$${totalGeral}`)
