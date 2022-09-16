b1379d8c5ae45f7c030925940fd343b41b040b52
leoespindula <espindulasystem.dev@gmail.com>
2022-09-16 11:40:11 -0300

    Update promise_1.js

--- funcoes/promise_1.js
+++ funcoes/promise_1.js
@@ -2,12 +2,15 @@ let a = 1
 console.log(a)
 
 let p = new Promise(function(cumprirPromessa) {
-    cumprirPromessa({
-        x:3,
-        y:4
-    })
+    cumprirPromessa(['Ana', 'Bia', 'Carlos', 'Daniel'])
 })
 
-p.then(function(valor){
-   console.log(valor.x, valor.y)
-})
+function primeiroElemento(array){
+    return array[0]
+}
+
+p
+    .then(valor => valor[0])
+    .then(primeiro => primeiro[0])
+    .then(letra => letra.toLowerCase())
+    .then(letraMinuscula => console.log(letraMinuscula))
