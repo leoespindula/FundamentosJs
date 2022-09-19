c3dd6546b9e4eb6863e85261fb546a73ea692f3f
leoespindula <espindulasystem.dev@gmail.com>
2022-09-19 10:08:21 -0300

    Update async_await_1.js

--- funcoes/async_await_1.js
+++ funcoes/async_await_1.js
@@ -6,20 +6,48 @@ function esperarPor(tempo = 2000){
     })
 }
 
-esperarPor(2000)
-    .then(()=> console.log('Executando promisse 1...'))
-    .then(esperarPor)
-    .then(()=> console.log('Executando promisse 2...'))
-    .then(esperarPor)
-    .then(()=> console.log('Executando promisse 3...'))
+// esperarPor(2000)
+//     .then(()=> console.log('Executando promisse 1...'))
+//     .then(esperarPor)
+//     .then(()=> console.log('Executando promisse 2...'))
+//     .then(esperarPor)
+//     .then(()=> console.log('Executando promisse 3...'))
 
-async function executar(){
-    await esperarPor(1500)
-    console.log('Async/Await 1...')
-    await esperarPor(1500)
-    console.log('Async/Await 2...')
-    await esperarPor(1500)
-    console.log('Async/Await 3...')
+function retornarValor() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 5000)
+    })
 }
 
-executar()
+async function retornarValorRapido() {
+    return 20
+}
+
+async function executar(){
+    let valor = await retornarValor()
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor}...`)
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 1}...`)
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 2}...`)
+
+    return valor + 3
+}
+
+async function executarDeVerdade() {
+    const valor = await executar()
+    console.log(valor)
+}
+
+// executar()
+executarDeVerdade()
+// async function executar(){
+//     let valor = await retornarValorRapido()
+//     await esperarPor(1500)
+//     console.log(`Async/Await ${valor}...`)
+//     await esperarPor(1500)
+//     console.log(`Async/Await ${valor + 1}...`)
+//     await esperarPor(1500)
+//     console.log(`Async/Await ${valor + 2}...`)
+// }
